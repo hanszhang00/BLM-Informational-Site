6a9419fe7f006c2e29e21e4b27db738c6f99bf6f
Hans Zhang <hans00@cs.washington.edu>
2020-08-12 23:28:43 -0700

    adding typewriter effect

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,7 @@ import Image from "gatsby-image";
 import { Link } from "gatsby";
 import { graphql, useStaticQuery } from "gatsby";
 import SocialLinks from "../constants/socialLinks";
+import Typical from "react-typical";
 
 const query = graphql`
   {
@@ -27,7 +28,19 @@ const Hero = () => {
     <header className='hero'>
       <div className='hero-center'>
         <h1>Black Lives Matter</h1>
-        <h4>the relevant resources to be more educated in the movement</h4>
+        {/* <h4>the relevant resources to be more educated in the movement</h4> */}
+        <Typical
+          steps={[
+            "become more educated about the history of BLM",
+            2000,
+            "take action to support the movement TODAY",
+            1500,
+            "help us contribute if you can!",
+            1000,
+          ]}
+          loop={Infinity}
+          wrapper='h4'
+        />
       </div>
     </header>
   );
